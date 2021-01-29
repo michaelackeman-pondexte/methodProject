92306c0782ccbb8bc5516a5f2db91c96b726dd56
michaelackerma <Michael.Ackerman-Pondexte@patelhs.org>
2021-01-29 17:21:52 +0000

    I added multiple methods, multiple variables are being received by the main class, and more

--- Main.java
+++ Main.java
@@ -7,8 +7,18 @@ class Main {
 
     System.out.println("Avatar name?");
     String avatarName = scan.next();
-
+    System.out.println("What level are you?");
+    int levels = scan.nextInt();
+  
     System.out.println(meetUser(avatarName));
+    
+    int userLevel = 32;
+
+    int newLevel = level(levels, userLevel);
+    System.out.println("After playing dungeon 6, your avatar is now at level " + newLevel);
+
+    boolean skill = newskill()
+    System.out.println(skill);
 
   }
 
@@ -18,7 +28,22 @@ class Main {
     return greet;
   }
 
-  int level()
+  static int level(int levels, int userLevel) 
+  {
+    int newLevel = userLevel + levels;
+    return newLevel; 
+  }
+
+  static boolean newSkill( int newLevel)
+  {
+    if(newLevel > 100)
+    {
+      Skill = "Due to your level being " + newLevel + ", you have unlocked Flurry Rush";
+    }else{
+      Skill = "Due to your level being " + newLevel + ", you still have Flurry";
+      return skill;
+    }
+  }
 
 
 }
