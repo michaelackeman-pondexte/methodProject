e1b9a0a445f01971d007c35887f47128bdad1375
michaelackerma <Michael.Ackerman-Pondexte@patelhs.org>
2021-02-02 14:52:45 +0000

    I added some comments

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ class Main {
     Scanner scan = new Scanner(System.in);
 
     System.out.println("Avatar name?");
-    String avatarName = scan.next();
+    String avatarName = scan.next();//This obtains the avatarName
     System.out.println("What level are you?");
-    int levels = scan.nextInt();
+    int levels = scan.nextInt();//This obtains the avatar's level and later is added with userlevel
   
     System.out.println(meetUser(avatarName));
     
@@ -24,26 +24,26 @@ class Main {
 
   static String meetUser(String avatarName)
   {
-    String greet = avatarName + " welcome back to Dungeon Crawler.";
-    return greet;
+    String greet = avatarName + " welcome back to Dungeon Crawler.";//This string of code recieves the avatarName and inserts it into the variable greet along with a string
+    return greet;//This returns the variable greet to the main method
   }
 
   static int level(int levels, int userLevel) 
   {
-    int newLevel = userLevel + levels;
-    return newLevel; 
+    int newLevel = userLevel + levels;//This adds two variables together to obtain newlevel
+    return newLevel;//this returns newlevel to the main method
   }
 
   static String newSkill(int newLevel)
   {
     String skill;
-    if(newLevel > 100)
+    if(newLevel > 100)//This if else statement checks if newlevel is over 100
     {
       skill = "Due to your level now being " + newLevel + ", you have unlocked Flurry Rush.";
     }else{
       skill = "Due to your level now being " + newLevel + ", you still have Flurry. Once your level  is greater than 100, you will unlock Flurry Rush";
     }
-    return skill;
+    return skill;//This sends skill back to my main method
   }
 
 
